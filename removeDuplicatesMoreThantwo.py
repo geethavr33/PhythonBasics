88ef8328ae99a4660e93418e9421ce98f8473829
geethavr33 <geetha.vr@vaisesika.local>
2024-10-15 12:41:11 +0530

    dat 5 task

---
+++ removeDuplicatesMoreThantwo.py
@@ -0,0 +1,20 @@
+def remove_excess_duplicates(nums):
+    if len(nums) <= 2:
+        return len(nums)  
+    i = 2 
+    for j in range(2, len(nums)):
+        if nums[j] != nums[i-2]:
+            nums[i] = nums[j]
+            i += 1
+    
+    for x in range(i, len(nums)):
+        nums[x] = '_'
+    
+    return i 
+
+
+nums = [1, 1, 1, 2, 2, 3,3,3]
+k = remove_excess_duplicates(nums)
+
+print(f"Number of valid elements: {k}")
+print(f"Modified array: {nums}")
